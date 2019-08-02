29dd352f7e2d45c5c1e7ed7a157327ef1313582f
Kevin Wynn <kevin@robo-house.com>
2019-08-02 18:14:12 -0500

    initial calendar work

--- src/views/Schedules.vue
+++ src/views/Schedules.vue
@@ -5,6 +5,9 @@
         <v-btn outlined class="mr-4" @click="setToday">
           Today
         </v-btn>
+        <v-btn color="primary" class="mr-4" @click="addNewCalendarEvent">
+          Add Event
+        </v-btn>
         <v-btn fab text small @click="prev">
           <v-icon small>mdi-arrow-left-drop-circle</v-icon>
         </v-btn>
@@ -32,9 +35,6 @@
             </v-list-item>
             <v-list-item @click="type = 'month'">
               <v-list-item-title>Month</v-list-item-title>
-            </v-list-item>
-            <v-list-item @click="type = '4day'">
-              <v-list-item-title>4 days</v-list-item-title>
             </v-list-item>
           </v-list>
         </v-menu>
@@ -72,16 +72,9 @@
             dark
           >
             <v-btn icon>
-              <v-icon>edit</v-icon>
+              <v-icon>mdi-calendar-edit</v-icon>
             </v-btn>
             <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn icon>
-              <v-icon>favorite</v-icon>
-            </v-btn>
-            <v-btn icon>
-              <v-icon>more_vert</v-icon>
-            </v-btn>
           </v-toolbar>
           <v-card-text>
             <span v-html="selectedEvent.details"></span>
@@ -102,17 +95,18 @@
 </template>
 
 <script>
+import moment from "moment";
+
 export default {
   name: "Schedules",
   data: () => ({
-    today: "2019-01-08",
-    focus: "2019-01-08",
+    today: moment().format("YYYY-MM-DD"),
+    focus: moment().format("YYYY-MM-DD"),
     type: "month",
     typeToLabel: {
       month: "Month",
       week: "Week",
-      day: "Day",
-      "4day": "4 Days"
+      day: "Day"
     },
     start: null,
     end: null,
@@ -121,129 +115,11 @@ export default {
     selectedOpen: false,
     events: [
       {
-        name: "Vacation",
-        details: "Going to the beach!",
-        start: "2018-12-29",
-        end: "2019-01-01",
-        color: "blue"
-      },
-      {
-        name: "Meeting",
-        details: "Spending time on how we do not have enough time",
-        start: "2019-01-07 09:00",
-        end: "2019-01-07 09:30",
-        color: "indigo"
-      },
-      {
-        name: "Large Event",
-        details:
-          "This starts in the middle of an event and spans over multiple events",
-        start: "2018-12-31",
-        end: "2019-01-04",
-        color: "deep-purple"
-      },
-      {
-        name: "3rd to 7th",
-        details: "Testing",
-        start: "2019-01-03",
-        end: "2019-01-07",
-        color: "cyan"
-      },
-      {
-        name: "Big Meeting",
-        details: "A very important meeting about nothing",
-        start: "2019-01-07 08:00",
-        end: "2019-01-07 11:30",
-        color: "red"
-      },
-      {
-        name: "Another Meeting",
-        details: "Another important meeting about nothing",
-        start: "2019-01-07 10:00",
-        end: "2019-01-07 13:30",
-        color: "brown"
-      },
-      {
-        name: "7th to 8th",
-        start: "2019-01-07",
-        end: "2019-01-08",
-        color: "blue"
-      },
-      {
-        name: "Lunch",
-        details: "Time to feed",
-        start: "2019-01-07 12:00",
-        end: "2019-01-07 15:00",
-        color: "deep-orange"
-      },
-      {
-        name: "30th Birthday",
-        details: "Celebrate responsibly",
-        start: "2019-01-03",
-        color: "teal"
-      },
-      {
-        name: "New Year",
-        details: "Eat chocolate until you pass out",
-        start: "2019-01-01",
-        end: "2019-01-02",
-        color: "green"
-      },
-      {
-        name: "Conference",
-        details: "The best time of my life",
-        start: "2019-01-21",
-        end: "2019-01-28",
-        color: "grey darken-1"
-      },
-      {
-        name: "Hackathon",
-        details: "Code like there is no tommorrow",
-        start: "2019-01-30 23:00",
-        end: "2019-02-01 08:00",
-        color: "black"
-      },
-      {
-        name: "event 1",
-        start: "2019-01-14 18:00",
-        end: "2019-01-14 19:00",
-        color: "#4285F4"
-      },
-      {
-        name: "event 2",
-        start: "2019-01-14 18:00",
-        end: "2019-01-14 19:00",
-        color: "#4285F4"
-      },
-      {
-        name: "event 5",
-        start: "2019-01-14 18:00",
-        end: "2019-01-14 19:00",
-        color: "#4285F4"
-      },
-      {
-        name: "event 3",
-        start: "2019-01-14 18:30",
-        end: "2019-01-14 20:30",
-        color: "#4285F4"
-      },
-      {
-        name: "event 4",
-        start: "2019-01-14 19:00",
-        end: "2019-01-14 20:00",
-        color: "#4285F4"
-      },
-      {
-        name: "event 6",
-        start: "2019-01-14 21:00",
-        end: "2019-01-14 23:00",
-        color: "#4285F4"
-      },
-      {
-        name: "event 7",
-        start: "2019-01-14 22:00",
-        end: "2019-01-14 23:00",
-        color: "#4285F4"
+        name: "yo yo yigg",
+        details: "this waddup",
+        start: moment().format("YYYY-MM-DD"),
+        end: moment().format("YYYY-MM-DD"),
+        color: "#4290b2"
       }
     ]
   }),
@@ -269,8 +145,6 @@ export default {
         case "month":
           return `${startMonth} ${startYear}`;
         case "week":
-        case "4day":
-          return `${startMonth} ${startDay} ${startYear} - ${suffixMonth} ${endDay} ${suffixYear}`;
         case "day":
           return `${startMonth} ${startDay} ${startYear}`;
       }
@@ -284,6 +158,9 @@ export default {
     }
   },
   methods: {
+    addNewCalendarEvent() {
+      console.log("hello");
+    },
     viewDay({ date }) {
       this.focus = date;
       this.type = "day";
